368e405447089be20debaf463b584f0082d36981
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-02-21 02:17:59 +0700

    feat: change title

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,13 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Former" },
+    { name: "description", content: "Create forms, hassle-free." },
+  ];
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,6 @@
 import Button from "~/components/button";
 import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
   return (
     <main>
