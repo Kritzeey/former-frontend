e760d577297e09e4d57cd782a75e2cf7860f6d30
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-02-21 04:47:09 +0700

    feat: add dummy data for forms

--- app/routes/forms/forms-list.tsx
+++ app/routes/forms/forms-list.tsx
@@ -1,30 +1,84 @@
 import FormCard from "~/components/form-card";
 
-let forms = [
+const forms = [
   {
     title: "Event Registration Form",
-    questions: 10,
+    questions: 4,
     responses: 2,
+    question_list: [
+      { id: 1, label: "Are you attending this event?", type: "boolean" },
+      {
+        id: 2,
+        label: "Are you willing to join this event from start to finish?",
+        type: "boolean",
+      },
+      {
+        id: 3,
+        label: "Do you have any dietary restrictions?",
+        type: "checkbox",
+      },
+      { id: 4, label: "T-shirt size", type: "dropdown" },
+    ],
   },
   {
     title: "Customer Satisfaction Survey",
-    questions: 10,
+    questions: 4,
     responses: 24,
+    question_list: [
+      { id: 1, label: "How would you rate your experience?", type: "scale" },
+      {
+        id: 2,
+        label: "What is the primary reason for your score?",
+        type: "paragraph",
+      },
+      { id: 3, label: "Would you recommend us to a friend?", type: "boolean" },
+      {
+        id: 4,
+        label: "Which product did you purchase?",
+        type: "multiple_choice",
+      },
+    ],
   },
   {
     title: "Short Quiz",
-    questions: 10,
+    questions: 4,
     responses: 56,
+    question_list: [
+      {
+        id: 1,
+        label: "What is the capital of France?",
+        type: "multiple_choice",
+      },
+      { id: 2, label: "Solve: 2 + 2 * 2", type: "short_answer" },
+      { id: 3, label: "Select all primary colors", type: "checkbox" },
+      { id: 4, label: "True or False: The earth is flat.", type: "boolean" },
+    ],
   },
   {
     title: "Volunteer Sign-Up",
-    questions: 10,
+    questions: 4,
     responses: 2,
+    question_list: [
+      { id: 1, label: "Full Name", type: "short_answer" },
+      { id: 2, label: "Availability (Days of the week)", type: "checkbox" },
+      { id: 3, label: "Previous volunteer experience", type: "paragraph" },
+      { id: 4, label: "Emergency contact number", type: "short_answer" },
+    ],
   },
   {
     title: "Quick Review Session",
-    questions: 10,
+    questions: 4,
     responses: 42,
+    question_list: [
+      { id: 1, label: "Was the material covered clearly?", type: "scale" },
+      {
+        id: 2,
+        label: "Which topic was most difficult?",
+        type: "multiple_choice",
+      },
+      { id: 3, label: "Do you need a follow-up session?", type: "boolean" },
+      { id: 4, label: "Any additional comments?", type: "paragraph" },
+    ],
   },
 ];
 
